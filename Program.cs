a25d136c8471f1c40c440d94608ce9de6e6fe561
bbhxwl <470138890@qq.com>
2020-05-19 03:19:04 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pubg_radar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
